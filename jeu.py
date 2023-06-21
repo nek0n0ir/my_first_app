1d4eb0a2bfddd982380ac29fd13131288474434b
François Lecourt <136647285+nek0n0ir@users.noreply.github.com>
2023-06-21 22:24:30 +0200

    Add new flag game

--- jeu.py
+++ jeu.py
@@ -1,45 +1,93 @@
 from flask import Flask, render_template, request, redirect, url_for
+from unidecode import unidecode
 import random
 import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
-def jouer():
-    conn = sqlite3.connect('pays.db')
+def accueil():
+    return render_template('accueil.html')
+
+@app.route('/jeu1')
+def jeu1():
+    conn = sqlite3.connect('fusion.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM pays")
-    pays = cursor.fetchall()
+    cursor.execute("SELECT * FROM fusion")
+    fusion = cursor.fetchall()
 
 
     # Sélectionner un pays aléatoire
-    pays_aleatoire = random.choice(pays)
+    pays_aleatoire = random.choice(fusion)
     conn.close()
-
 
     nom_pays = pays_aleatoire[0]
     drapeau = pays_aleatoire[3]
 
     # Rediriger vers la route de vérification avec le nom du pays en tant que paramètre
-    return render_template('verifier.html', nom_pays=nom_pays, drapeau=drapeau)
+    return render_template('jeu1.html', nom_pays=nom_pays, drapeau=drapeau)
 
-@app.route('/verifier', methods=['POST'])
-def verifier():
+@app.route('/verif1', methods=['POST'])
+def verif1():
     nom_pays = request.form['nom_pays']
     capital_saisie = request.form['capitale']
     
-    conn = sqlite3.connect('pays.db')
+    conn = sqlite3.connect('fusion.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT capitale FROM pays WHERE nom_pays=?", (nom_pays,))
+    cursor.execute("SELECT CAPITALE FROM fusion WHERE NOM=?", (nom_pays,))
     capital_reel = cursor.fetchone()[0]
     conn.close()
 
-    if capital_saisie == capital_reel:
+    # Normalisation des chaînes de caractères en enlevant les accents
+    capital_saisie = unidecode(capital_saisie)
+    capital_reel = unidecode(capital_reel)
+
+    if capital_saisie.lower() == capital_reel.lower():
         resultat = "Correct !"
     else:
         resultat = "Incorrect ! La capitale est " + capital_reel
 
-    return render_template('resultat.html', resultat=resultat)
+    return render_template('verif1.html', resultat=resultat)
+
+@app.route('/jeu2')
+def jeu2():
+    conn = sqlite3.connect('fusion.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM fusion")
+    fusion = cursor.fetchall()
+
+
+    # Sélectionner un pays aléatoire
+    pays_aleatoire = random.choice(fusion)
+    conn.close()
+
+    nom_pays = pays_aleatoire[0]
+    drapeau = pays_aleatoire[3]
+
+    # Rediriger vers la route de vérification avec le nom du pays en tant que paramètre
+    return render_template('jeu2.html', nom_pays=nom_pays, drapeau=drapeau)
+
+@app.route('/verif2', methods=['POST'])
+def verif2():
+    nom_pays = request.form['nom_pays']
+    pays_saisie = request.form['pays']
+    
+    conn = sqlite3.connect('fusion.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT NOM FROM fusion WHERE NOM=?", (nom_pays,))
+    pays_reel = cursor.fetchone()[0]
+    conn.close()
+
+    # Normalisation des chaînes de caractères en enlevant les accents
+    pays_saisie = unidecode(pays_saisie)
+    pays_reel = unidecode(pays_reel)
+
+    if pays_saisie.lower() == pays_reel.lower():
+        resultat = "Correct !"
+    else:
+        resultat = "Incorrect ! La pays est " + pays_reel
+
+    return render_template('verif2.html', resultat=resultat)
 
 if __name__ == '__main__':
     app.run(debug=True)
